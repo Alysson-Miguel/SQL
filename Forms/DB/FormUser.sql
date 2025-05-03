6a9ca8249d01a6f90f83a9520491ee717176f596
Alysson-Miguel <Edicao.Dash@gmail.com>
2025-05-03 13:10:06 -0300

    Criando Formulario

---
+++ Forms/DB/FormUser.sql
@@ -0,0 +1,8 @@
+/*CREATE TABLE formUser(
+	id_User INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	user_name VARCHAR(49),
+	user_date DATE,
+	user_email VARCHAR(49),
+	user_password VARCHAR(49)
+)*/
+
