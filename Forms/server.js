6127ae44d6e36355c65b7353ee34338ef5108cba
Alysson-Miguel <Edicao.Dash@gmail.com>
2025-05-05 15:11:06 -0300

    Adição do mysql12

--- Forms/server.js
+++ Forms/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2'); 
+const path = require('path');
+
 
 const app = express();
 const port = 3000;
-
 
 app.use(bodyParser.json());
 
@@ -12,7 +13,7 @@ const db = mysql.createConnection({
   host: '127.0.0.1', 
   port: '3308,',
   user: 'root',
-  password: 'admin',
+  password: 'admim',
   database: 'querystest'
 });
 
@@ -32,7 +33,15 @@ app.post('/api/save-data', (req, res) => {
   const userData = req.body;
 
   const sql = 'INSERT INTO formuser (user_name,user_date, user_email, user_password)';
-  db.query(sql, [userData.name, userData.date, userData.email, userData.password])
+  db.query(sql, [userData.name, userData.date, userData.email, userData.password], (err, result) => {
+    if (err) {
+      console.error('Erro ao inserir dados:', err);
+      res.status(500).json({ message: 'Erro ao salvar os dados no banco de dados.' });
+    } else {
+      console.log('Dados inseridos com sucesso:', result);
+      res.status(200).json({ message: 'Dados salvos com sucesso no banco de dados!' });
+    }
+  });
 });
 
 app.listen(port, () => {
