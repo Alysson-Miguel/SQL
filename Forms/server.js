3ff8bf320e074ec6e1b5ed3c1792e58c263a8f64
Alysson-Miguel <Edicao.Dash@gmail.com>
2025-05-05 15:09:22 -0300

    Adição do Express

--- Forms/server.js
+++ Forms/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2'); 
-const path = require('path');
-
 
 const app = express();
 const port = 3000;
+
 
 app.use(bodyParser.json());
 
